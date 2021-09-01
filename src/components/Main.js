21c72f31e49883a67b9e8901055840fae6c4af51
ymcla <ymclapp@yahoo.com>
2021-09-01 10:31:59 -0500

    added the features.  Can't get pics to show yet

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import React from 'react'; 
 import HornedBeasts from './HornedBeasts';
+import longhorned from './longhorned.jpg';
+import triceratops from './triceratops.jpg';
 
 // class component 
 
@@ -9,8 +11,14 @@ export default class Main extends React.Component {
           // return whatever you want to render 
     return ( 
       <div>
-     <HornedBeasts title = "One Horn" imageURL = "image" description = "This is a one horned beast.  Beware!" />
-     <HornedBeasts title = "Three Horn" imageURL = "image" description = "This is a three horned beast.  Hear me roar" />
+     <HornedBeasts 
+     title = "Two Horn" 
+     imageURL = {longhorned} alt = "Long Horned"
+     description = "This is a two horned beast.  Beware!" />
+     <HornedBeasts 
+     title = "Three Horn" 
+     imageURL = {triceratops} alt = "Triceratops" 
+     description = "This is a three horned beast.  Hear me roar" />
      </div>
     ) 
   } 
